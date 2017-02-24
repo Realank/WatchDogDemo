3f53e7854477ab56c615de5723e0e0af6c0429d7
Realank <realank@126.com>
2017-02-24 08:52:50 +0800

    add proj

---
+++ WatchDogDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatchDogDemo
+//
+//  Created by Realank on 2017/2/24.
+//  Copyright © 2017年 Realank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WatchDogDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchDogDemo
+//
+//  Created by Realank on 2017/2/24.
+//  Copyright © 2017年 Realank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
